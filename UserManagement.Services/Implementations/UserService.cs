daa1041fe5a82dfddfd11d955d41bda4c746e0cd
NickDucker <nickduckr@live.com>
2024-02-29 20:47:18 +0000

    CRUD action methods added.

--- UserManagement.Services/Implementations/UserService.cs
+++ UserManagement.Services/Implementations/UserService.cs
@@ -18,4 +18,40 @@ public class UserService(IDataContext dataAccess) : IUserService
 
     // Method to fetch and list all users.
     public IEnumerable<User> GetAll() => _dataAccess.GetAll<User>();
+
+    public void CreateUser(User user)
+    {
+        _dataAccess.Create(user);
+    }
+    // Nullable because might not return a user if the id does not match.
+    public User? GetUserById(int userId)
+    {
+        return _dataAccess.GetAll<User>().FirstOrDefault(u => u.Id == userId);
+    }
+    public bool DeleteUserById(int userId)
+    {
+        var user = _dataAccess.GetAll<User>().FirstOrDefault(u => u.Id == userId);
+        if (user == null) return false;
+        _dataAccess.Delete(user);
+        return true;
+    }
+    public void UpdateUser(User user)
+    {
+        var existingUser = _dataAccess.GetAll<User>().FirstOrDefault(u => u.Id == user.Id);
+        if (existingUser != null)
+        {
+            // Update the properties of the existing user entity.
+            existingUser.Forename = user.Forename;
+            existingUser.Surname = user.Surname;
+            existingUser.Email = user.Email;
+            existingUser.DateOfBirth = user.DateOfBirth;
+            existingUser.IsActive = user.IsActive;
+            // Apply changes by calling Update function and passing in new user entity with updated properties.
+            _dataAccess.Update(existingUser);
+        }
+        else
+        {
+            throw new KeyNotFoundException("User not found.");
+        }
+    }
 }
